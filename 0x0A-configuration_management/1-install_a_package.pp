a3067fbfedd461c90d3e510c9a85a518e436d581
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-04-24 05:54:07 -0700

    task 1-- fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,15 @@
-# installs flask using pip3
-
+# install flask using pip3
 
 package { 'flask':
-  ensure   => '2.1.0',
-  name     => 'flask',
-  provider => 'pip3'
+    ensure   => '2.1.0',
+    name     => 'flask',
+    provider => 'pip3'
+}
+
+package { 'werkzeug':
+    ensure   => '1.0.1',
+    name     => 'Werkzeug',
+    provider => 'pip3',
+    require  => Package['flask'],
 }
 
